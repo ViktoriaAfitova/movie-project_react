29b4ec1f416dee4b250086c94ffc036e9acacf12
AfitovaViktoriya <afitovaviktoria@gmail.com>
2022-03-01 15:05:21 +0300

    feat: add movieDetails component

--- movie-app/src/components/MovieList/MovieList.js
+++ movie-app/src/components/MovieList/MovieList.js
@@ -1,7 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
+import MovieDetails from '../MovieDetails/MovieDetails';
 import div from './movieList.css';
 
 const MovieList = (props) => {
+  const [showDetails, setDetails] = useState(false);
 
   return (
 		<div className="movie-container">
@@ -9,11 +11,17 @@ const MovieList = (props) => {
         <div className="card" key={movie.id} style={{"width": "18rem"}}>
           <div className="row">
             <img src={movie.Poster} className="card-img-top" alt="movie"></img>
-            <div className="overlay"><span className="details">Details</span>
+            <div className="overlay" onClick={() => setDetails(true)}><span className="details">Details</span>
             </div>
           </div>
         </div>
       ))}
+      <MovieDetails
+        visible={showDetails}
+        onCancel={() => setDetails(false)}
+        closeBtnShow
+      >
+      </MovieDetails>
     </div>
 	)
 }

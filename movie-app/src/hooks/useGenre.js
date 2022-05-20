e8c937e04b1af4b9e0496ebd1d4b781ad7f8708c
AfitovaViktoriya <afitovaviktoria@gmail.com>
2022-05-20 14:04:08 +0300

    Updates

--- movie-app/src/hooks/useGenre.js
+++
@@ -1,9 +0,0 @@
-const useGenres = (selectedGenre) => {
-  if (selectedGenre.length < 1)
-    return '';
-
-  const GenreIDs = selectedGenre.map((g) => g.id)
-	return GenreIDs.reduce((acc, curr) => acc + ',' + curr)
-}
-
-export default useGenres;
